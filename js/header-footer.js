c1b8908776f1214f53fb06c6c7d8cb68a0408445
kojiwada-at-hokkaidoo <149213483+kojiwada-at-hokkaidoo@users.noreply.github.com>
2023-10-28 09:52:50 +0900

    Add files via upload

--- js/header-footer.js
+++ js/header-footer.js
@@ -1,6 +1,6 @@
 //共通パーツ読み込み
 $(function() {
-    $("#header").load("https://kojiwada-at-hokkaidoo.github.io/website/footer.html");
+    $("#header").load("https://kojiwada-at-hokkaidoo.github.io/website/header.html");
     // $("#header").load("../header.html");
     $("#footer").load("https://kojiwada-at-hokkaidoo.github.io/website/footer.html");
     // $("#footer").load("../footer.html");
