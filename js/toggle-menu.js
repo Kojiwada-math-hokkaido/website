4b953dcadc343ab12b0f8457ff6ccbf5eaae55e1
kojiwada-at-hokkaidoo <149213483+kojiwada-at-hokkaidoo@users.noreply.github.com>
2024-02-26 16:01:31 +0900

    Add files via upload

--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -1,6 +1,28 @@
+// window.addEventListener('load', function () {
+//   var $button = document.querySelector('.toggle-menu-button');
+//   var $menu = document.querySelector('.header-site-menu');
+//   $button.addEventListener('click', function () {
+//     if ($menu.classList.contains('is-show')) {
+//       $menu.classList.remove('is-show');
+//     }
+//     else {
+//       $menu.classList.add('is-show');
+//     }
+//   });
+// });
+
 window.addEventListener('load', function () {
   var $button = document.querySelector('.toggle-menu-button');
   var $menu = document.querySelector('.header-site-menu');
+  // ボタンを無効に設定
+  $button.setAttribute('disabled', 'disabled');
+  // ボタンをクリック可能にする関数
+  function enableButton() {
+    $button.removeAttribute('disabled');
+  }
+  // ページの読み込みが完了したらボタンを有効にする
+  enableButton();
+
   $button.addEventListener('click', function () {
     if ($menu.classList.contains('is-show')) {
       $menu.classList.remove('is-show');
@@ -8,5 +30,6 @@ window.addEventListener('load', function () {
     else {
       $menu.classList.add('is-show');
     }
+  });
 });
-});
+
