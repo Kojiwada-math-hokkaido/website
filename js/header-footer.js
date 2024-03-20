532115064f8b6807b53c196348307f0df76324cf
Koji Wada <149213483+Kojiwada-math-hokkaido@users.noreply.github.com>
2024-03-20 15:21:21 +0900

    Delete js directory

--- js/header-footer.js
+++
@@ -1,9 +0,0 @@
-//共通パーツ読み込み
-$(function() {
-    $("#header").load("https://kojiwada-at-hokkaidoo.github.io/website/header.html");
-    // $("#header").load("../header.html");
-    $("#footer").load("https://kojiwada-at-hokkaidoo.github.io/website/footer.html");
-    // $("#footer").load("../footer.html");
-});
-
-
--- js/item-list.js
+++
@@ -1,5 +0,0 @@
-//共通パーツ読み込み
-$(function() {
-    $("#item-list").load("https://kojiwadaofhokkaido.github.io/website00/main_html/item-list.html");
-    //$("#item-list").load("../main_html/item-list.html");
-});
