ceea9851de6339f4bf6030d191bc824f626b4d5a
Koji Wada <149213483+Kojiwada-math-hokkaido@users.noreply.github.com>
2024-03-20 16:03:10 +0900

    Add files via upload

--- js/header-footer.js
+++ js/header-footer.js
@@ -1,8 +1,8 @@
 // //共通パーツ読み込み
 $(function() {
-    $("#header").load("https://kojiwada-math-hokkaido.github.io/website/header.html");
+    $("#header").load("https://Kojiwada-math-hokkaido.github.io/website/header.html");
     // $("#header").load("../header.html");
-    $("#footer").load("https://kojiwada-math-hokkaido.github.io/website/footer.html");
+    $("#footer").load("https://Kojiwada-math-hokkaido.github.io/website/footer.html");
     // $("#footer").load("../footer.html");
 });
 
