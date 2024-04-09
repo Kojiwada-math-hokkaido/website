307c35e21e934aaafc878acedc6982d8fb26274a
Koji Wada <149213483+Kojiwada-math-hokkaido@users.noreply.github.com>
2024-04-10 03:34:00 +0900

    Add files via upload

--- js/loading.js
+++ js/loading.js
@@ -1,7 +1,44 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // 2 秒後に Loading... を非表示にしてコンテンツを表示する
-  setTimeout(function() {
-      document.getElementById("loading").style.display = "none"; // Loading... を非表示に
-      document.getElementById("content").style.display = "block"; // コンテンツを表示
-  }, 1500); // 2 秒待つ
-});
+// $(function () {
+//   function end_loader() {
+//     $('.loader').fadeOut(800);
+//     document.getElementById("content").style.display = "block"; // コンテンツを表示
+//   }
+//   $(window).on('load', function () {
+//     setTimeout(function () {
+//       end_loader();
+//     }, 2900)
+//   })
+// })
+
+
+$(function () {
+  // ローダー終了
+  function end_loader() {
+    $('.loader').fadeOut(800);
+    document.getElementById("content").style.display = "block"; // コンテンツを表示
+  }
+  // テキスト表示
+  function show_txt() {
+    $('.loader .txt').fadeIn(400);
+  }
+  // テキスト非表示
+  function hide_txt() {
+    $('.loader .txt').fadeOut(400);
+  }
+
+  // タイマー処理
+  $(window).on('load', function () {
+    // 処理①
+    setTimeout(function () {
+      show_txt();
+    }, 500)
+    // 処理②
+    setTimeout(function () {
+      hide_txt();
+    }, 3500)
+    // 処理③
+    setTimeout(function () {
+      end_loader();
+    }, 3900)
+  })
+})
